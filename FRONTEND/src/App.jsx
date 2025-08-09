b5747478902e2a74c44cf3d9c550d3561db6e8b9
omprakash kumar <omprakashkumarglb@gmail.com>
2025-08-09 20:22:02 +0530

    retry again

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { API_BASE } from "./config";
+import { API_BASE } from "../config.js";
 
 function App() {
   const [message, setMessage] = useState("");
